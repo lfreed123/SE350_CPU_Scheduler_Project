c71d8c8ec265637d6606106580f89b38bbccdc3b
Bored1615 <lfreed952@gmail.com>
2026-07-09 14:13:49 -0500

    initial skeleton code and beginning of Processing states

---
+++ src/model/Process.java
@@ -0,0 +1,32 @@
+package model;
+
+
+/**
+ * Class representing a process to be scheduled. But no real logic of schedling lives here.
+ */
+
+public class Process {
+    private final int id;
+    private final int placeholder1;
+    private final int placeholder2;
+    private final int priority;
+
+
+public Process(int id, int placeholder1, int placeholder2, int priority) {
+    this.id = id;
+    this.placeholder1 = placeholder1;
+    this.placeholder2 = placeholder2;
+    this.priority = priority;
+}   
+
+public int getId() {
+        return id;
+    }
+
+// Other functions will be listed here
+
+public int getPriority() {
+        return priority;
+    }
+
+}
